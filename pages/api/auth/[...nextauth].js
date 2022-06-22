4dfa07603e52dba1ec926f6b58d652c15682deed
Pratik Sharma <sharma.pratik2016@gmail.com>
2022-06-22 22:30:48 +0530

    added the nextjs change

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,12 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+const prisma = new PrismaClient();
 export default NextAuth({
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
